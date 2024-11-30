83549534f85b16997dfe515f23c178763f82ecac
Fatim-coding <timabadjie12@gmail.com>
2024-11-30 15:48:01 +0000

    second commit

---
+++ main.py
@@ -0,0 +1,8 @@
+var = 10            #initialise
+while var > 0:      #iterate loop
+    var = var - 1 
+    if var == 7:      #condition 1
+        continue      #condition statement
+    #display result
+    print ('\ncurrent variable value :', var)
+print ("\nGood bye!")
